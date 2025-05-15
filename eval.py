294f0bd73ea29ea170720826dae98a040fba683c
Guanwei <756522391@qq.com>
2025-05-15 16:12:51 +0800

    update

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 max_content_len = 100
 max_seq_len = 128
 batch_size = 32
-dataset_name = 'BGL'   # 'Thunderbird' 'HDFS_v1'  'BGL'  'Liberty‘
+dataset_name = 'Liberty'   # 'Thunderbird' 'HDFS_v1'  'BGL'  'Liberty‘
 data_path = r'/mnt/public/gw/SyslogData/{}/test.csv'.format(dataset_name)
 
 Bert_path = r"/mnt/public/gw/LLM_model/bert-base-uncased"
